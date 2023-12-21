a2af63c0dd0db6f45a84b05c2a301838fc4d4e7d
Elwynn Shino 01 <144208181+blackswordsman01@users.noreply.github.com>
2023-12-21 12:47:21 +0400

    Elwynn's Vending Machine

--- vendingmachine.py
+++ vendingmachine.py
@@ -1,5 +1,6 @@
 class VendingMachine:
     def __init__(self):
+        # Initialize the vending machine with a menu, stock, and money variables
         self.menu = {
             "1": {"item": "Snickers Bar", "price": 1.50, "stock": 5},
             "2": {"item": "Water Bottle", "price": 1.00, "stock": 10},
@@ -14,15 +15,18 @@ class VendingMachine:
         self.total_change = 0.0
 
     def display_heading(self):
+        # Display a welcome message
         print("""𝓦𝓮𝓵𝓬𝓸𝓶𝓮 𝓽𝓸 𝓔𝓵𝔀𝔂𝓷𝓷'𝓼 𝓥𝓮𝓷𝓭𝓲𝓷𝓰 𝓜𝓪𝓬𝓱𝓲𝓷𝓮""")
 
     def display_menu(self):
+        # Display the menu items along with their prices and stock status
         print("\nMENU:")
         for code, item in self.menu.items():
             stock_status = "" if item["stock"] > 0 else "(Sold Out)"
             print(f"{code}: {item['item']} - ${item['price']:.2f} {stock_status}")
 
     def input_money(self):
+        # Prompt the user to insert money and handle invalid inputs
         while True:
             try:
                 self.money_inserted += float(input("Insert money: "))
@@ -31,6 +35,7 @@ class VendingMachine:
                 print("Invalid input. Please enter a valid amount.")
 
     def input_code(self):
+        # Prompt the user to enter the code of the item they want to purchase
         while True:
             code = input("Enter the code of the item you want to purchase (Type 'no' to stop): ").upper()
             if code == 'NO':
@@ -43,12 +48,15 @@ class VendingMachine:
                 print("Invalid code. Please enter a valid code.")
 
     def dispense_item(self, item):
+        # Display a message indicating the item being dispensed
         print(f"\nDispensing {item}...")
 
     def calculate_change(self, money_inserted, item_price):
+        # Calculate and return the change after a purchase
         return money_inserted - item_price
 
     def suggest_purchase(self, item):
+        # Provide a suggestion based on the purchased item
         suggestions = {
             'Cold Coffee': 'How about adding a snack like Cheez-its?',
             'Honest Tea': 'Consider pairing it with Goldfish Pretzels.',
@@ -58,11 +66,13 @@ class VendingMachine:
         return suggestions.get(item, '')
 
     def run(self):
+        # Run the vending machine
         self.display_heading()
-        self.display_menu()  # Display the menu first
-        self.input_money()  # Ask the user to insert money after seeing the menu
+        self.display_menu()  
+        self.input_money()  
         initial_money = self.money_inserted
-        while True:
+        continue_purchase = True  
+        while continue_purchase:
             code = self.input_code()
             if code == 'NO':
                 break
@@ -81,9 +91,12 @@ class VendingMachine:
             if all(item["stock"] == 0 for item in self.menu.values()):
                 print("Sorry, the vending machine is out of stock.")
                 break
-            self.money_inserted = change  # Use the remaining money for the next purchase
+            self.money_inserted = change  
+            # Ask the user if they wish to continue their purchase after the first individual purchase
+            user_input = input("Do you wish to continue your purchase? (Type 'yes' to continue or 'no' to end): ").upper()
+            continue_purchase = user_input == 'YES'
         remaining_money = self.money_inserted
-        self.money_inserted = initial_money  # Reset money_inserted to the initial value
+        self.money_inserted = initial_money  
         print(f"Total change: ${remaining_money:.2f}")
         print("\nThank you for using the Vending Machine!")
 
